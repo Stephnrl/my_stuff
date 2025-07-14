562e73f3ca2378d8834ecacf41140163fad9c7a5
Stephnrl <45036564+Stephnrl@users.noreply.github.com>
2025-07-14 18:45:08 -0400

    Update space.ps1

--- space.ps1
+++ space.ps1
@@ -3,3 +3,14 @@ Get-ChildItem C:\ -Recurse -File -ErrorAction SilentlyContinue |
     Sort-Object Length -Descending | 
     Select-Object FullName, @{Name="SizeGB";Expression={[math]::Round($_.Length/1GB,3)}}, LastWriteTime |
     Format-Table -AutoSize
+
+
+Get-ChildItem C:\ -Directory | ForEach-Object {
+    $size = (Get-ChildItem $_.FullName -Recurse -ErrorAction SilentlyContinue | 
+             Measure-Object -Property Length -Sum).Sum
+    [PSCustomObject]@{
+        Folder = $_.Name
+        SizeGB = [math]::Round($size / 1GB, 2)
+        SizeMB = [math]::Round($size / 1MB, 2)
+    }
+} | Sort-Object SizeGB -Descending | Format-Table -AutoSize
