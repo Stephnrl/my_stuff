6f0f6152f5177b73a76b5e0bbd9c33986b770d69
Stephnrl <45036564+Stephnrl@users.noreply.github.com>
2025-08-18 16:39:18 -0400

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -8,37 +8,10 @@ SELECT
 FROM sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, 'DETAILED') ips
 INNER JOIN sys.indexes i ON ips.object_id = i.object_id AND ips.index_id = i.index_id
 WHERE ips.last_update > DATEADD(day, -7, GETDATE())
+    AND OBJECT_NAME(ips.object_id) NOT LIKE 'Azure%'  -- Exclude Azure system tables
+    AND OBJECT_NAME(ips.object_id) IN (
+        SELECT TABLE_NAME 
+        FROM INFORMATION_SCHEMA.TABLES 
+        WHERE TABLE_SCHEMA = 'dbo'
+    )
 ORDER BY ips.last_update DESC;
-
-
-SELECT 
-    t.name AS TableName,
-    s.name AS StatName,
-    STATS_DATE(s.object_id, s.stats_id) AS LastUpdated,
-    s.auto_created,
-    s.user_created
-FROM sys.stats s
-INNER JOIN sys.tables t ON s.object_id = t.object_id
-WHERE STATS_DATE(s.object_id, s.stats_id) > DATEADD(day, -7, GETDATE())
-ORDER BY STATS_DATE(s.object_id, s.stats_id) DESC;
-
-
-EXEC xp_readerrorlog 0, 1, N'rebuild', N'index'
-EXEC xp_readerrorlog 0, 1, N'statistics', N'update'
-
-
-
-
-
-
-SELECT 
-    operation,
-    context,
-    [transaction id],
-    [begin time],
-    [transaction name]
-FROM fn_dblog(NULL, NULL)
-WHERE operation IN ('LOP_BEGIN_XACT', 'LOP_COMMIT_XACT')
-    AND [begin time] > DATEADD(hour, -48, GETDATE())
-    AND [transaction name] LIKE '%maintenance%'
-ORDER BY [begin time] DESC;
