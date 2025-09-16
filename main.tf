73614935c3f0809b4fdd7411d0ff4ca24614c630
Stephnrl <45036564+Stephnrl@users.noreply.github.com>
2025-09-16 14:30:56 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,128 +1,37 @@
-# variables.tf - Input variables for the Azure VM ARM template module
+# outputs.tf - Output values from the Azure VM ARM template deployment
 
-variable "resource_group_name" {
-  description = "Name of the resource group where the VM will be deployed"
-  type        = string
+output "vm_id" {
+  description = "Resource ID of the deployed virtual machine"
+  value       = try(local.deployment_outputs.vmId.value, "")
 }
 
-variable "vm_name" {
-  description = "Name of the virtual machine"
-  type        = string
+output "private_ip_address" {
+  description = "Private IP address of the VM"
+  value       = try(local.deployment_outputs.privateIpAddress.value, "")
 }
 
-variable "location" {
-  description = "Azure region for the VM deployment"
-  type        = string
+output "public_ip_address" {
+  description = "Public IP address of the VM (if public IP was created)"
+  value       = try(local.deployment_outputs.publicIpAddress.value, "")
 }
 
-variable "vm_size" {
-  description = "Size of the virtual machine (must support Gen2 and Trusted Launch)"
-  type        = string
-  default     = "Standard_D4s_v5"
+output "nic_id" {
+  description = "Resource ID of the network interface"
+  value       = try(local.deployment_outputs.nicId.value, "")
 }
 
-variable "admin_username" {
-  description = "Admin username for the VM"
-  type        = string
-  default     = "azureuser"
+output "os_disk_id" {
+  description = "Resource ID of the OS disk"
+  value       = try(local.deployment_outputs.osDiskId.value, "")
 }
 
-variable "admin_password" {
-  description = "Admin password (required if SSH key not provided)"
-  type        = string
-  default     = ""
+output "deployment_id" {
+  description = "ID of the ARM template deployment"
+  value       = azurerm_resource_group_template_deployment.vm.id
+}
+
+output "deployment_output_content" {
+  description = "Raw output content from the ARM deployment (for debugging)"
+  value       = azurerm_resource_group_template_deployment.vm.output_content
   sensitive   = true
-}
-
-variable "ssh_public_key" {
-  description = "SSH public key for authentication (recommended over password)"
-  type        = string
-  default     = ""
-}
-
-variable "subnet_id" {
-  description = "Resource ID of the subnet for the VM's NIC"
-  type        = string
-}
-
-variable "compute_gallery_image_id" {
-  description = "Resource ID of the Compute Gallery Image Version (RHEL 9 Gen2 with Trusted Launch)"
-  type        = string
-  # Example format: /subscriptions/{subscription-id}/resourceGroups/{rg}/providers/Microsoft.Compute/galleries/{gallery}/images/{image}/versions/{version}
-}
-
-variable "os_disk_size_gb" {
-  description = "Size of the OS disk in GB"
-  type        = number
-  default     = 128
-}
-
-variable "os_disk_type" {
-  description = "Storage type for the OS disk"
-  type        = string
-  default     = "Premium_LRS"
-  validation {
-    condition = contains([
-      "Standard_LRS",
-      "Premium_LRS",
-      "StandardSSD_LRS",
-      "Premium_ZRS",
-      "StandardSSD_ZRS"
-    ], var.os_disk_type)
-    error_message = "Invalid OS disk type specified."
-  }
-}
-
-variable "data_disks" {
-  description = "List of data disk configurations"
-  type = list(object({
-    name                = string
-    diskSizeGB          = number
-    lun                 = number
-    storageAccountType  = string
-  }))
-  default = []
-  # Example:
-  # [
-  #   {
-  #     name               = "data1"
-  #     diskSizeGB        = 256
-  #     lun               = 0
-  #     storageAccountType = "Premium_LRS"
-  #   },
-  #   {
-  #     name               = "data2"
-  #     diskSizeGB        = 512
-  #     lun               = 1
-  #     storageAccountType = "StandardSSD_LRS"
-  #   }
-  # ]
-}
-
-variable "enable_accelerated_networking" {
-  description = "Enable accelerated networking on the VM's NIC"
-  type        = bool
-  default     = true
-}
-
-variable "use_public_ip" {
-  description = "Whether to create and attach a public IP address"
-  type        = bool
-  default     = false
-}
-
-variable "availability_zone" {
-  description = "Availability zone for the VM (1, 2, 3, or empty for no zone)"
-  type        = string
-  default     = ""
-  validation {
-    condition = var.availability_zone == "" || contains(["1", "2", "3"], var.availability_zone)
-    error_message = "Availability zone must be 1, 2, 3, or empty."
-  }
-}
-
-variable "tags" {
-  description = "Tags to apply to all resources"
-  type        = map(string)
-  default     = {}
 }
