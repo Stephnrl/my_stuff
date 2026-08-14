0c5c3859173d9b3028c8f6ffb50a184573e66001
Stephnrl <45036564+Stephnrl@users.noreply.github.com>
2026-08-14 13:57:05 -0400

    Delete tgw directory

--- tgw/mnt/user-data/outputs/terraform-aws-network/modules/tgw-routing/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "attachment_id" {
-  description = "The attachment ID being routed."
-  value       = local.attachment_id
-}
-
-output "association_route_table_id" {
-  description = "Route table the attachment is associated with, if any."
-  value       = var.associate_route_table_id
-}
--- tgw/mnt/user-data/outputs/terraform-aws-network/modules/tgw-spoke/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "attachment_id" {
-  description = "Transit gateway VPC attachment ID. The hub needs this for the tgw-routing module."
-  value       = aws_ec2_transit_gateway_vpc_attachment.this.id
-}
-
-output "vpc_owner_id" {
-  description = "Account ID that owns the attached VPC."
-  value       = aws_ec2_transit_gateway_vpc_attachment.this.vpc_owner_id
-}
